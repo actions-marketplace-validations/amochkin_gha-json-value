79499fa35bb6192e203a160cfd90ee405c9f2222
amochkin <amochkin@gmail.com>
2022-11-11 19:07:16 -0500

    Initial commit

--- src/action.ts
+++ src/action.ts
@@ -7,7 +7,7 @@ const workspace = process.env.GITHUB_WORKSPACE ?? './';
 
 export const run = () => {
 	try {
-		const file = core.getInput('file') ?? path.join(workspace, 'package.json');
+		const file = path.join(workspace, core.getInput('file') ?? 'package.json');
 		const jsonPath = core.getInput('property').split('.');
 		const jsonObject = JSON.parse(fs.readFileSync(file).toString());
 
